b70058670a307d3a6f69428cc1a4b50dc226ddc1
andrey <applofilll@gmail.com>
2020-05-04 23:00:18 +0300

    fix laba4

--- laba4/ODU.h
+++ laba4/ODU.h
@@ -1,41 +1,13 @@
-//
-// Created by 17491433 on 04.05.2020.
-//
-
-#ifndef LABA4_ODU_H
-#define LABA4_ODU_H
+﻿#ifndef ODE_H
+#define ODE_H
 
 #include <vector>
-#include <cmath>
+#include <functional>
 
-using namespace std;
+std::vector<std::vector<double>> pikar(const std::vector<std::function<double(const double, const std::vector<double>&)>>& f,const std::vector<double>& alpha,double t0,double tm,double step,bool isParallel);
+double metric(const std::vector<double>& x, const std::vector<double>& y, bool is_parallel);
+std::vector<double> calc(const std::function<double(const double)>& u, double t0, double tm, double step);
+std::vector<std::vector<double>> transposition(const std::vector<std::vector<double>>& matrix);
+double maxMetric(const std::vector<double>& x, const std::vector<double>& y);
 
-class ODU {
-    private:
-        int size;
-    public:
-        ODU(int size);
-        ~ODU();
-        int getSize();
-        vector<vector<double>> pikar(const std::vector<std::function<double(const double, const std::vector<double>&)>>& f,
-                             const std::vector<double>& alpha,
-                             const double t0,
-                             const double tm,
-                             const double step,
-                             const bool is_parallel);
-    // Функция для получения значений функции u на отрезке [t0; tm]
-        std::vector<double> calculate(
-                const std::function<double(const double)>& u,
-                const double t0,
-                const double tm,
-                const double step);
-
-    // Реализация операции транспонирования
-        std::vector<std::vector<double>> transpose(const std::vector<std::vector<double>>& matrix);
-
-    // Реализация метрики, основанной на макимум-норме
-        double maximumMetric(const std::vector<double>& x, const std::vector<double>& y);
-};
-
-
-#endif //LABA4_ODU_H
+#endif
--- laba4/main.cpp
+++ laba4/main.cpp
@@ -1,117 +1,67 @@
-#include <iostream>
+﻿#include <iostream>
 #include <omp.h>
-
+#include <cmath>
 #include "ODU.h"
 
-
-// Функции ui для тестового примера
-
 double u1(const double t) {
-    return t * t + 2 * t + 1;
+  return t + 2 * t + 1;
 }
-
-
-
 double u2(const double t) {
-    return t + 1;
+  return t;
 }
-
-
-
 double u3(const double t) {
-    return log(t) + t * t;
+  return log(t) + t;
 }
-
-
-
 double u4(const double t) {
-    return sin(t);
+  return -sin(t);
 }
-
-
-
 double u5(const double t) {
-    return cos(t);
+  return cos(t);
 }
-
-// Функции fi для тестового примера
 
 double f1(const double t, const std::vector<double>& u) {
-    return 2 * u[1];
+  return u[1];
 }
-
-
-
 double f2(const double t, const std::vector<double>& u) {
-    return u[3] * u[3] + u[4] * u[4];
+  return u[3] + u[4] * u[2];
 }
-
-
-
 double f3(const double t, const std::vector<double>& u) {
-    return 1 / (u[1] - 1) + 2 * t;
+  return 1 / u[1] + t;
 }
-
-
-
 double f4(const double t, const std::vector<double>& u) {
-    return u[4];
+  return u[4];
 }
-
-
-
 double f5(const double t, const std::vector<double>& u) {
-    return -u[3];
+  return -u[3] + u[2];
 }
 
 
 
 int main() {
-    using namespace std;
+  using namespace std;
 
-    vector<function<double(const double, const vector<double>&)>> f{ f1, f2, f3, f4, f5 };
-    double t0(0.1);
-    double tm(10.1);
-    vector<double> alpha{ u1(t0), u2(t0), u3(t0), u4(t0), u5(t0) };
-    double step(0.001);
-    ODU odu(5);
+  vector<function<double(const double, const vector<double>&)>> f{ f1, f2, f3, f4, f5 };
+  double t0(0.1);
+  double tm(10.1);
+  vector<double> alpha{ u1(t0), u2(t0), u3(t0), u4(t0), u5(t0) };
+  double step(0.001);
 
+  // Найдём значение коэффициента ускорения
+  double time_sequental(0.0);
+  double time_parallel(0.0);
+  double start(0.0);
+  for (ptrdiff_t i(0); i < 5; i += 1) {
+    start = omp_get_wtime();
+      pikar(f, alpha, t0, tm, step, false);
+    time_sequental += omp_get_wtime() - start;
 
-    // Найдём численное решение системы ОДУ в последовательном и параллельном режиме
-    vector<vector<double>> solved1(odu.pikar(f, alpha, t0, tm, step, false));
-    vector<vector<double>> transposed_solved1(odu.transpose(solved1));
-    vector<vector<double>> solved2(odu.pikar(f, alpha, t0, tm, step, true));
-    vector<vector<double>> transposed_solved2(odu.transpose(solved2));
+    start = omp_get_wtime();
+      pikar(f, alpha, t0, tm, step, true);
+    time_parallel += omp_get_wtime() - start;
+  }
+  cout << "coef: " << time_sequental / time_parallel << endl;
 
-    vector<function<double(const double)>> u{ u1, u2, u3, u4, u5 };
-    vector<double> calculated;
-    for (ptrdiff_t i_func(0); i_func < 5; i_func += 1) {
-        calculated = odu.calculate(u[i_func], t0, tm, step);
-        // Максимальное значение модуля разницы между точными значениями
-        // и значениями численного решения при последовательном подходе
-        cout << "Consequential\nu" << i_func + 1 << ": " << odu.maximumMetric(calculated, transposed_solved1[i_func]) << endl;
-        // Максимальное значение модуля разницы между точными значениями
-        // и значениями численного решения при параллельном подходе
-        cout << "Parallel\nu" << i_func + 1 << ": " << odu.maximumMetric(calculated, transposed_solved2[i_func]) << endl << endl;
-    }
-    cout << endl;
-
-    // Найдём значение коэффициента ускорения
-    double time_sequental(0.0);
-    double time_parallel(0.0);
-    double start(0.0);
-    for (ptrdiff_t i(0); i < 5; i += 1) {
-        start = omp_get_wtime();
-        odu.pikar(f, alpha, t0, tm, step, false);
-        time_sequental += omp_get_wtime() - start;
-
-        start = omp_get_wtime();
-        odu.pikar(f, alpha, t0, tm, step, true);
-        time_parallel += omp_get_wtime() - start;
-    }
-    cout << "Coefficient: " << time_sequental / time_parallel << endl;
-
-    char stop('0');
-    cin >> stop;
-    return 0;
+  char stop('0');
+  cin >> stop;
+  return 0;
 }
